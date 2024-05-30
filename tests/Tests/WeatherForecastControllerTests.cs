8931232c188561c5b032c2e190a06dad79ed733d
Brad McDavid <brad.mcdavid@gmail.com>
2024-05-30 14:38:41 -0400

    adding weather api (#1)

---
+++ tests/Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using App.Api.Features.Forecast;
+using Microsoft.Extensions.Logging;
+
+namespace App.Tests;
+
+public class WeatherForecastControllerTests
+{
+    private Mock<ILogger<WeatherForecastController>> logger = new();
+
+    [Fact]
+    public void Test1()
+    {
+        var controller = new WeatherForecastController(logger.Object);
+
+        var result = controller.Get();
+
+        result.Count().Should().Be(5);
+    }
+}
