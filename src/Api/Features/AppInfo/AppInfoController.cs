702155025ddf793553af34c113b9451a54f88256
Brad McDavid <brad.mcdavid@gmail.com>
2024-06-12 14:04:25 -0400

    Adding App Info Api (#6)
    * Adding App Info Api
    
    * Adding Tests
    
    ---------
    
    Co-authored-by: brad.mcdavid@thestudioc.org <brad.mcdavid@thestudioc.org>

---
+++ src/Api/Features/AppInfo/AppInfoController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Api.Features.AppDetails;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AppInfoController : ControllerBase
+{
+    [HttpGet]
+    public Task<IReadOnlyCollection<string>> Get()
+    {
+        var items = new List<string>();
+        var buildInfo = Weather.Contracts.BuildInfoAttribute.BuildInfoAttributes;
+
+        foreach (var (key, value) in buildInfo)
+        {
+            items.Add($"build, {key} = {value}");
+        }
+
+        var environmentVariables = Environment.GetEnvironmentVariables()
+            .OfType<DictionaryEntry>()
+            .OrderBy(o => o.Key);
+
+        foreach (var envItem in environmentVariables)
+        {
+            var maskedValue = envItem.Value is string { Length: > 0 } s
+                ? string.Concat(Enumerable.Range(0, s.Length).Select(s => '*'))
+                : "***";
+            items.Add($"env, {envItem.Key} = {maskedValue}");
+        }
+
+        return Task.FromResult<IReadOnlyCollection<string>>(items);
+    }
+}
